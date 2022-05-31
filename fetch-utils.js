c459e1de5506d5751abf9a8cae3c5b618b80ced9
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-05-31 10:43:56 -0700

    removed email notification from supabase

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://sdlwjpqahpjeifwgzwuj.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkbHdqcHFhaHBqZWlmd2d6d3VqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM2OTI3MjcsImV4cCI6MTk2OTI2ODcyN30.ZrHNlkqxRcpyMPe1vBxWLCKVBqUSLAPj5cek4R35NSg';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -10,12 +10,12 @@ export function getUser() {
 export function checkAuth() {
     const user = getUser();
 
-    if (!user) location.replace('../');
+    if (!user) location.replace('/auth');
 }
 
 export function redirectIfLoggedIn() {
     if (getUser()) {
-        location.replace('./other-page');
+        location.replace('../game');
     }
 }
 
@@ -34,7 +34,7 @@ export async function signInUser(email, password) {
 export async function logout() {
     await client.auth.signOut();
 
-    return (window.location.href = '../');
+    return (window.location.href = '/');
 }
 
 // function checkError({ data, error }) {
