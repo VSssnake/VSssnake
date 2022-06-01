cd955777a0b08d3044e0facb678cfde79f1b25f4
KASHI TAMANG <tamang.kashi@gmail.com>
2022-06-01 09:59:29 -0700

    adds game logic skeleton

--- game/game.js
+++ game/game.js
@@ -5,7 +5,8 @@ const logoutButton = document.getElementById('logout');
 const formEl = document.getElementById('form');
 //  -grab score-counter 
 const scoreEl = document.getElementById('score-count');
-
+const promptEl = document.getElementById('promptEl');
+// const inputEl = document.getElementById('username-input');
 
 logoutButton.addEventListener('click', async () => {
     await logout();
@@ -16,54 +17,44 @@ logoutButton.addEventListener('click', async () => {
 
 // });
 
+
+let score = 0;
+//checking for command cut line
+let currentState = 1;
+
 formEl.addEventListener('submit', async (e) => {
     e.preventDefault();
     const form = new FormData(formEl);
     // this includes a test input
-    await createPlayerData({ user_name: form.get('username-input'), user_score: form.get('test') });
+    await createPlayerData({ user_name: form.get('username-input'), user_score: score });
+    window.location.href = '/';
 });
-
-
-let score = 0;
-
 
 function startGame() {
     document.addEventListener('keydown', event => {
+        event.preventDefault();
         //verify correct answer
-        if (event.metaKey && event.code === 'KeyX'){
-                    //alert user if correct
+        if (currentState === 1 && event.metaKey && event.code === 'KeyX'){
+        //alert user if correct
             alert('you commanded ⌘X to cut a line!');
         //update the state
             score++;
         //update the dom view
+            promptEl.textContent = 'prompt 2: on your keyboard, enter the shortcut to paste a whole line in VsCode';
+            scoreEl.textContent = score;
+            currentState++;
+        } else if (currentState === 2 && event.metaKey && event.code === 'KeyV'){
+            alert ('you command the paste command!');
+            score++;
             scoreEl.textContent = score;
         }
         //create brand new if state and do the same if else alert error
     });
 }
+
+//do a click event listener for the end of the game to toggle startGame()?
 startGame();
 
-// saveGame();
-//grab score + username
-//push to supabase 
-//redirect to home 
-
-
-//this is the function we were working on with TAS
-
-// function startGame() {
-//     document.addEventListener('keydown', event => {
-//         //verify correct answer
-//         if (event.metaKey && event.code === 'KeyX'){
-//                     //alert user if correct
-//             alert('you commanded ⌘X to cut a line!');
-//         //update the state
-//             score++;
-//         //update the dom view
-//             scoreEl.textContent = score;
-//         } else {
-//             alert('wronk!');
-//         }
-//         //create brand new if state and do the same if else alert error
-//     });
-//}
+// if (inputEl.addEventListener('click', () => {
+//     return;
+// })
