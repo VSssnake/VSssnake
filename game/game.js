2e90f23ab79b5fdb2558699c9b4b293952f3b5dd
KASHI TAMANG <tamang.kashi@gmail.com>
2022-05-31 17:41:06 -0700

    adds point when event

--- game/game.js
+++ game/game.js
@@ -3,6 +3,7 @@ import { logout, createPlayerData } from '../fetch-utils.js';
 const logoutButton = document.getElementById('logout');
 // const deleteAccount = document.getElementById('delete-data');
 const formEl = document.getElementById('form');
+//  -grab score-counter 
 const scoreEl = document.getElementById('score-count');
 
 
@@ -22,23 +23,32 @@ formEl.addEventListener('submit', async (e) => {
     await createPlayerData({ user_name: form.get('username-input'), user_score: form.get('test') });
 });
 
-//  -grab score-counter 
-// -let state
 // -render function for prompt div
 // -on document event handler
 // -if user input = answer, state++, alert 'u got it right' else, return alert 'ur wrong'
 
-
+// -let state
 let score = 0;
 
 
 function startGame() {
     document.addEventListener('keydown', event => {
-        if (event.metaKey && event.code === 'KeyX') {
-            alert('you commanded ⌘X to cut a line!');
-        }
-    });    
+        //verify correct answer
+        event.metaKey && event.code === 'KeyX';
+        //alert user if correct
+        alert('you commanded ⌘X to cut a line!');
+        //update the state
+        score++;
+        //update the dom view
+        scoreEl.textContent = score;
+        //validate in console
+        console.log(score);
+    });
 }
 
 startGame();
 
+// saveGame();
+//grab score + username
+//push to supabase 
+//redirect to home 
